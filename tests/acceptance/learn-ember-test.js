692dc30f5f20e1105244f733d498d826deeecdf7
saurass <saurabhsrivastava312@gmail.com>
2019-01-26 14:16:50 +0530

    writing test for route check click
    Signed-off-by: saurass <saurabhsrivastava312@gmail.com>

--- tests/acceptance/learn-ember-test.js
+++ tests/acceptance/learn-ember-test.js
@@ -1,13 +1,17 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import {
+  click,
+  currentURL,
+  visit
+} from '@ember/test-helpers';
 
 module('Acceptance | learn ember', function(hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /', async function(assert) {
-    await visit('/');
-
-    assert.equal(currentURL(), '/');
+  test('visiting /about', async function(assert) {
+    await visit('/about');
+    await click('.button')
+    assert.equal(currentURL(), '/contact', 'should navigate equal to /contact');
   });
 });
